7bef98c29262663fc7c55ca9fca31f57b55274e6
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-06-18 10:49:43 -0700

    added null check

--- printf.c
+++ printf.c
@@ -14,7 +14,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL)
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 	for (i = 0; format[i]; i++)
@@ -34,5 +34,8 @@ int _printf(const char *format, ...)
 		else
 			k += _putchar(format[i]);
 	}
+
+	va_end(args);
+
 	return (k);
 }
